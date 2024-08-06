6506cdd79350af7b46ef8abb186ec32a7bca2eef
Toma Morishita <25winterhorse@gmail.com>
2024-08-06 22:47:47 +0900

    名前付きルート

---
+++ resources/js/Pages/InertiaTest.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { Link } from '@inertiajs/inertia-vue3';
+
+</script>
+
+<template>
+Inertiaテスト<br>
+<a href="/">aタグから来ました</a><br>
+<Link href="/">Linkから来ました</Link>
+<Link :href="route('inertia.index')">名前付きrouteの確認</Link>
+
+</template>
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+use App\Http\Controllers\InertiaTestController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,6 +16,13 @@ use Inertia\Inertia;
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+Route::get('/intertia-test', function () {
+    return Inertia::render('InertiaTest');
+    }
+);
+
+Route::get('/intertia/index', [InertiaTestController:: class, 'index'])->name('inertia.index'); 
 
 Route::get('/', function () {
     return Inertia::render('Welcome', [
