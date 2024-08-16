29219d7a9279e354f3280626b6044b9c4adc6263
Toma Morishita <25winterhorse@gmail.com>
2024-08-17 01:12:32 +0900

    イベントコールバック

--- app/Http/Controllers/InertiaTestController.php
+++ app/Http/Controllers/InertiaTestController.php
@@ -10,7 +10,9 @@ class InertiaTestController extends Controller
 {
     public function index()
     {
-        return Inertia::render('Inertia/Index');
+        return Inertia::render('Inertia/Index', [
+            'blogs' => InertiaTest::all()
+        ]);
     }
 
     public function create()
@@ -23,7 +25,8 @@ class InertiaTestController extends Controller
         // dd($id);
         return Inertia::render('Inertia/Show',
         [
-            'id' => $id
+            'id' => $id,
+            'blog' => InertiaTest::findOrFail($id)
         ]);
     }
 
@@ -42,6 +45,17 @@ class InertiaTestController extends Controller
         return to_route('inertia.index')
         ->with([
             'message' => '登録しました。'
+        ]);
+    }
+
+    public function delete($id)
+    {
+        $book = InertiaTest::findOrFail($id);
+        $book -> delete();
+
+        return to_route('inertia.index')
+        ->with([
+            'message' => '削除しました。'
         ]);
     }
 }
--- resources/js/Pages/Inertia/Index.vue
+++ resources/js/Pages/Inertia/Index.vue
@@ -1,9 +1,19 @@
-<script>
+<script setup>
+import { Link } from '@inertiajs/inertia-vue3';
+import { ref } from 'vue'
+defineProps({
+    blogs: Array
+})
 </script>
 
 <template>
     <div v-if="$page.props.flash.message" class="bg-blue-300">
         {{ $page.props.flash.message }}
     </div>
-    あああああああああ
+    <ul>
+        <li v-for="blog in blogs" :key="blog.id">
+            <Link class="text-blue-400" :href="route('inertia.show', { id:blog.id })">件名： {{ blog.title }}</Link>, <br>
+            本文： {{ blog.content }}
+        </li>
+    </ul>
 </template>
