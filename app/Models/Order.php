13c98da38b651b5313ca6f1fc35aad5fee92ba57
Toma Morishita <25winterhorse@gmail.com>
2024-10-14 23:32:27 +0900

    chart作成

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\Scopes\Subtotal;
+use Carbon\Carbon;
 
 class Order extends Model
 {
@@ -14,4 +15,35 @@ class Order extends Model
     {
         static::addGlobalScope(new Subtotal);
     }
+
+    public function scopeBetweenDate($query, $startDate = null, $endDate = null)
+    {
+        // 開始日と終了日が両方とも指定されていない場合、元のクエリをそのまま返す
+        if (is_null($startDate) && is_null($endDate)) 
+        {
+            return $query;
+        }
+
+        // 開始日のみが指定されている場合、開始日以降のデータを取得
+        if (!is_null($startDate) && is_null($endDate)) 
+        {
+            return $query->where('created_at', '>=', $startDate);
+        }
+
+        // 終了日のみが指定されている場合、終了日以前のデータを取得
+        if (is_null($startDate) && !is_null($endDate))
+        {
+            $endDate1 = Carbon::parse($endDate)->addDays(1);
+            return $query->where('created_at', '<=', $endDate1);
+        }
+
+        // 開始日と終了日が両方指定されている場合、期間内のデータを取得
+        if (!is_null($startDate) && !is_null($endDate)) 
+        {
+            $endDate1 = Carbon::parse($endDate)->addDays(1);
+            return $query->where('created_at', '>=', $startDate)
+                        ->where('created_at', '<=', $endDate1);
+        }
+    }
+
 }
