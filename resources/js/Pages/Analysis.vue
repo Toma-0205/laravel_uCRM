4735de01817966d657d4d2af948165e19b590cba
Toma Morishita <25winterhorse@gmail.com>
2024-10-16 01:11:53 +0900

    分析結果をデータから表示

--- resources/js/Pages/Analysis.vue
+++ resources/js/Pages/Analysis.vue
@@ -29,6 +29,8 @@ const getData = async () => {
         })
         .then( res => {
             data.data = res.data.data
+            data.labels = res.data.labels
+            data.totals = res.data.totals
             console.log(res.data)
         })
     } catch(e) {
@@ -55,7 +57,9 @@ const getData = async () => {
                             To:<input type="date" name="endDate" v-model="form.endDate"><br>
                             <button class="mt-4 flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">分析する</button>
                         </form>
-                        <Chart />
+                        <div v-show="data.data">
+                            <Chart :data="data"/>
+                        </div>
                         <div v-show="data.data" class="lg:w-2/3 w-full mx-auto overflow-auto">
                             <table class="table-auto w-full text-left whitespace-no-wrap">
                                 <thead>
