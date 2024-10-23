862ac6947b5af1e719dd2d8b128a87620e2151f9
Toma Morishita <25winterhorse@gmail.com>
2024-10-24 01:06:12 +0900

    RFM分析を追加していく

--- app/Http/Controllers/AnalysisController.php
+++ app/Http/Controllers/AnalysisController.php
@@ -12,9 +12,28 @@ class AnalysisController extends Controller
 {
     public function index()
     {
-        $startDate = '2022-08-01';
+        $startDate = '2021-09-01';
         $endDate = '2022-08-31';
     
+        
+
+        // RFM分析
+        // 1.IDごと
+        $subQuery = Order::betweenDate($startDate, $endDate)
+        ->groupBy('id')
+        ->selectRaw('id, customer_id, customer_name, SUM(subtotal) as totalPerPurchase, created_at');
+
+        // 2.顧客ごとに最終購入日、購入回数、購入合計金額を取得
+        $subQuery = DB::table($subQuery)
+        ->groupBy('customer_id')
+        ->selectRaw('customer_id, customer_name, 
+        max(created_at) as recentDate, 
+        datediff(now(), max(created_at)) as recency, 
+        count(customer_id) as frequency, 
+        sum(totalPerPurchase) as monetary')->get();
+
+        dd($subQuery);
+
         return Inertia::render('Analysis');
     }
 }
